bd38d7bab43d6364756a8a1f0e1343b0c60477a2
carterli0128 <118191819+carterli0128@users.noreply.github.com>
2024-07-16 14:52:06 +0800

    Add files via upload

--- poisson_.lean
+++ poisson_.lean
@@ -1,3 +1,19 @@
-/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
-import Mathlib.Data.Real.Basic
-import Library.Basic
+import mathlib.data.set.basic
+import mathlib.measure_theory.measure
+import mathlib.measure_theory.probability_space
+
+namespace probability
+
+structure SampleSpace (Ω : Type) where
+  toType : Ω
+
+structure EventSpace (Ω : Type) where
+  toSet : set Ω
+
+structure ProbabilitySpace (Ω : Type) (F : EventSpace Ω) where
+  toFun : F.toSet → ℝ
+  empty_prob : toFun ∅ = 0
+  full_prob : toFun (set.univ : set Ω) = 1
+  countable_additive : ∀ (A B : set Ω), (∀ i, i ∈ A → i ∉ B) → toFun (A ∪ B) = toFun A + toFun B
+
+end probability
